fc5e4e70a196d4653e9e1f981fb716c00c0b23ee
grma16021 <groth.hansen.mathias@gmail.com>
2017-11-20 21:43:24 +0100

    Add files via upload

---
+++ Fib.py
@@ -0,0 +1,17 @@
+# Print the fibonacci sequense
+# By Mathias
+#
+#
+#
+#
+y = 1
+x = 0
+#Loop that Calculates the next numbers in the sequence
+while y <= 222232244629420445529739893461909967206666939096499764990979600:
+	print (x, y)
+	x = x + y
+	y = y + x
+print ("That's the first 300 numbers in the series")
+
+
+		
